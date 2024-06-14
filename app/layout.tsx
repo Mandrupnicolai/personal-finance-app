deeedd9452256c1a12ecf15bc429bb3d24827331
Mandrupnicolai <nicolai.uhre.com@gmail.com>
2024-06-14 14:52:08 +0200

    Created a POST method for the accounts. Added sheets and forms also to make the ui better.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider} from '@clerk/nextjs'
 import "./globals.css";
+import { Toaster } from "sonner";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +23,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <QueryProvider>
+            <SheetProvider />
+            <Toaster />
             {children}
           </QueryProvider>
         </body>
